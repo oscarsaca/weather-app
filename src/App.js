c79b02153b5fb2f6bdef00845b1ed46f34debf09
Oscar Saca <oscarsaca88@gmail.com>
2021-02-01 09:11:15 -0800

    Attempted to make api call. Currently we are recieving an error back in our response.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import WeatherBg from './components/weather-bg';
 import PageTitle from './components/page-title';
 import WeatherInfoContainer from './components/weather-info/weather-info-container';
 
+      
+const fetchData = () => {
+  const url = "api.openweathermap.org/data/2.5/forecast?id=524901&appid=a1a5b6654de7c4687ddd592a0d166af5";
+  return fetch(url)
+    .then(response => response.json())
+    .then(parsedJSON => console.log(parsedJSON.results))
+    .catch(error => console.log(`error: ${error}`));
+}
+
+fetchData()
+
 function App() {
   return (
     <React.StrictMode>
